fixed off-by-one error

models.py
@@ -29,12 +29,12 @@ class _SLDSMixin(object):
 
     def resample_dynamics_distns(self):
         for state, d in enumerate(self.dynamics_distns):
-            d.resample([s.strided_gaussian_states[s.stateseq[1:] == state]
+            d.resample([s.strided_gaussian_states[s.stateseq[:-1] == state]
                 for s in self.states_list])
         self._clear_caches()
 
     def resample_emission_distns(self):
-        # TODO get rid of this hstack
+        # TODO get rid of this hstack by adding separate x,y args to Regression
         for state, d in enumerate(self.emission_distns):
             d.resample([np.hstack((
                 s.gaussian_states[s.stateseq == state],
@@ -60,6 +60,9 @@ class HSMMSLDS(_SLDSMixin,pyhsmm.models.HSMM):
 
 
 class WeakLimitHDPHMMSLDS(_SLDSMixin,pyhsmm.models.WeakLimitHDPHMM):
+    _states_class = HMMSLDSStatesEigen
+
+class WeakLimitStickyHDPHMMSLDS(_SLDSMixin,pyhsmm.models.WeakLimitStickyHDPHMM):
     _states_class = HMMSLDSStatesEigen
 
 class WeakLimitHDPHSMMSLDS(_SLDSMixin,pyhsmm.models.WeakLimitHDPHSMM):