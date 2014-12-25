ddb7d7f1f9132c429dbcf1fb8dcdb9b7db0f242e
Matthew Johnson <mattjj@csail.mit.edu>
2014-12-24 17:29:40 -0800

    cleanup, add functions condition_on, predict, and sample_gaussian

--- lds.pyx
+++ lds.pyx
@@ -14,6 +14,7 @@ from blas_lapack cimport dsymm, dcopy, dgemm, dpotrf, \
         dgemv, dpotrs, daxpy, dtrtrs, dsyrk, dtrmv
 
 # TODO make switching version
+# TODO make kalman smoother
 # TODO make cholesky update/downdate versions
 # TODO make (generate?) single-precision version
 
@@ -181,135 +182,78 @@ def filter_and_sample(
 
     return np.asarray(randseq)
 
-###
+### util
 
-def condition_on(
+cdef inline void condition_on(
     # inputs
     double[::1] mu_x, double[::1,:] sigma_x,
-    double[::1,:] A, double[::1,:] sigma_states,
-    double[::1,:] C, double[::1,:] sigma_obs,
-    double[::1] y,
+    double[::1,:] A, double[::1,:] sigma_obs, double[::1] y,
     # outputs
     double[::1] mu_cond, double[::1,:] sigma_cond,
-    double[::1] mu_predict, double[::1,:] sigma_predict,
+    # temps
+    double[::1] temp_p, double[::1,:] temp_pn, double[::1,:] temp_pp,
     ):
-    cdef int n = mu_x.shape[0]
-    cdef int p = C.shape[0]
+    cdef int n = mu_x.shape[0], p = y.shape[0]
     cdef int nn = n*n, pp = p*p
-    cdef double one = 1., zero = 0., neg1 = -1.
     cdef int inc = 1, info = 0
+    cdef double one = 1., zero = 0., neg1 = -1.
 
-    cdef double[::1,:] sigma_y = np.empty((p,p),order='F')
-    cdef double[::1,:] temp1 = np.empty((p,n),order='F')
-    cdef double[::1] temp2 = np.empty((p,),order='F')
-    cdef double[::1,:] temp4 = np.empty((n,n),order='F')
+    # NOTE: this routine could actually call predict (a pxn version)
 
-    ### sigma_y = chol(sigma_obs + C.dot(sigma_x).dot(C.T))
-    ## temp1 = C.dot(sigma_x)
-    dgemm('N', 'N', &p, &n, &n, &one, &C[0,0], &p, &sigma_x[0,0], &n, &zero, &temp1[0,0], &p)
-    # dsymm('R','L', &p, &n, &one, &sigma_x[0,0], &n, &C[0,0], &p, &zero, &temp1[0,0], &p)
-    ## sigma_y = sigma_obs
-    dcopy(&pp, &sigma_obs[0,0], &inc, &sigma_y[0,0], &inc)
-    ## sigma_y += temp1.dot(C.T)
-    dgemm('N', 'T', &p, &p, &n, &one, &temp1[0,0], &p, &C[0,0], &p, &one, &sigma_y[0,0], &p)
-    ## sigma_y = chol(sigma_y)
-    dpotrf('L', &p, &sigma_y[0,0], &p, &info)
+    dgemm('N', 'N', &p, &n, &n, &one, &A[0,0], &p, &sigma_x[0,0], &n, &zero, &temp_pn[0,0], &p)
+    # dsymm('R','L', &p, &n, &one, &sigma_x[0,0], &n, &A[0,0], &p, &zero, &temp_pn[0,0], &p)
+    dcopy(&pp, &sigma_obs[0,0], &inc, &temp_pp[0,0], &inc)
+    dgemm('N', 'T', &p, &p, &n, &one, &temp_pn[0,0], &p, &A[0,0], &p, &one, &temp_pp[0,0], &p)
+    dpotrf('L', &p, &temp_pp[0,0], &p, &info)
 
-    ### temp2 = y - C * mu_x
-    ## temp2 = y
-    dcopy(&p, &y[0], &inc, &temp2[0], &inc)
-    ## temp2 -= C * mu_x
-    dgemv('N', &p, &n, &neg1, &C[0,0], &p, &mu_x[0], &inc, &one, &temp2[0], &inc)
-
-    ### mu_cond = mu_x + temp1' * solve_from_chol(sigma_y, temp2)
-    ## temp2 = solve(sigma_y, temp2)
-    dpotrs('L', &p, &inc, &sigma_y[0,0], &p, &temp2[0], &p, &info)
-    ## mu_cond = mu_x
+    dcopy(&p, &y[0], &inc, &temp_p[0], &inc)
+    dgemv('N', &p, &n, &neg1, &A[0,0], &p, &mu_x[0], &inc, &one, &temp_p[0], &inc)
+    dpotrs('L', &p, &inc, &temp_pp[0,0], &p, &temp_p[0], &p, &info)
     dcopy(&n, &mu_x[0], &inc, &mu_cond[0], &inc)
-    ## mu_cond += temp1.dot(temp2)
-    dgemv('T', &p, &n, &one, &temp1[0,0], &p, &temp2[0], &inc, &one, &mu_cond[0], &inc)
+    dgemv('T', &p, &n, &one, &temp_pn[0,0], &p, &temp_p[0], &inc, &one, &mu_cond[0], &inc)
 
-    ### sigma_cond = sigma_x - temp1.T.dot(sigma_y^{-1}).temp1
-    ## temp1 = solve_triangular(sigma_y, temp1)
-    dtrtrs('L', 'N', 'N', &p, &n, &sigma_y[0,0], &p, &temp1[0,0], &p, &info)
-    ## sigma_cond = sigma_x
+    dtrtrs('L', 'N', 'N', &p, &n, &temp_pp[0,0], &p, &temp_pn[0,0], &p, &info)
     dcopy(&nn, &sigma_x[0,0], &inc, &sigma_cond[0,0], &inc)
-    ## sigma_cond -= temp1.T.dot(temp1)
-    dgemm('T', 'N', &n, &n, &p, &neg1, &temp1[0,0], &p, &temp1[0,0], &p, &one, &sigma_cond[0,0], &n)
-    # dsyrk('L','T', &n, &p, &neg1, &temp1[0,0], &p, &one, &sigma_cond[0,0], &n)
+    dgemm('T', 'N', &n, &n, &p, &neg1, &temp_pn[0,0], &p, &temp_pn[0,0], &p, &one, &sigma_cond[0,0], &n)
+    # dsyrk('L','T', &n, &p, &neg1, &temp_pn[0,0], &p, &one, &sigma_cond[0,0], &n)
 
-    ### mu_predict = A.dot(mu_cond)
-    dgemv('N', &n, &n, &one, &A[0,0], &n, &mu_cond[0], &inc, &zero, &mu_predict[0], &inc)
-
-    ### sigma_predict = A.dot(sigma_cond).dot(A.T) + sigma_states
-    ## temp4 = A.dot(sigma_cond)
-    dgemm('N', 'N', &n, &n, &n, &one, &A[0,0], &n, &sigma_cond[0,0], &n, &zero, &temp4[0,0], &n)
-    # dsymm('R','L',&n, &n, &one, &sigma_cond[0,0], &n, &A[0,0], &n, &zero, &temp4[0,0], &n)
-    # sigma_predict = sigma_states
-    dcopy(&nn, &sigma_states[0,0], &inc, &sigma_predict[0,0], &inc)
-    # sigma_predict += temp4.dot(A.T)
-    dgemm('N', 'T', &n, &n, &n, &one, &temp4[0,0], &n, &A[0,0], &n, &one, &sigma_predict[0,0], &n)
-
-### TEST
-
-def test_dgemm(double[::1,:] A, double[::1,:] B, double[::1,:] C):
-    cdef int i, m = A.shape[0], n = B.shape[1], k = A.shape[1]
-    cdef double one = 1., zero = 0.
-
-    for i in range(1000):
-        dgemm('N','N', &m, &n, &k,
-                &one, &A[0,0], &m, &B[0,0], &k,
-                &zero, &C[0,0], &m)
-
-def test_dsymm(double[::1,:] A, double[::1,:] B, double[::1,:] C):
-    cdef int i, m = A.shape[0], n = B.shape[0]
-    cdef double one = 1., zero = 0.
-
-    for i in range(1000):
-        dsymm('L','L', &m, &n,
-                &one, &A[0,0], &m, &B[0,0], &m,
-                &zero, &C[0,0], &m)
-
-
-def foo(double[:,::1] A, double[::1] x, double[::1] out):
-    r_gemv(A,x,out)
-
-def foo2(double[:,::1] A, double[:,::1] B, double[:,::1] out):
-    r_gemm(A,B,out)
-
-def foo3(double[:,::1] A, double[::1] x, double[::1] out):
-    r_symv("L",A,x,out)
-
-def foo4(double[:,::1] A, double[:,::1] B, double[:,::1] out):
-    r_symm("L","L",A,B,out)
-
-
-def qform1(double[:,::1] X, double[:,::1] A, double[:,::1] out):
-    cdef int i
-    for i in range(1000):
-        matrix_qform(A.shape[0], A.shape[1], &A[0,0], &X[0,0], 0., &out[0,0])
-
-# TODO whoops A is assumed to be coming in transposed here!
-def qform2(double[:,::1] X, double[:,::1] A, double[:,::1] temp, double[:,::1] out):
-    cdef int j
-    cdef double alpha = 1., beta = 0.
+cdef inline void predict(
+    # inputs
+    double[::1] mu, double[::1,:] sigma,
+    double[::1,:] A, double[::1,:] sigma_states,
+    # outputs
+    double[::1] mu_predict, double[::1,:] sigma_predict,
+    # temps
+    double[::1,:] temp_nn,
+    ):
+    cdef int n = mu.shape[0]
+    cdef int nn = n*n
     cdef int inc = 1
+    cdef double one = 1., zero = 0.
 
-    # NOTE: tricky because row-major; everything is flipped!
+    dgemv('N', &n, &n, &one, &A[0,0], &n, &mu[0], &inc, &zero, &mu_predict[0], &inc)
 
-    for j in range(1000):
-        dsymm('R', 'L', <int*> &A.shape[1], <int*> &A.shape[0],
-                &alpha, &X[0,0], <int*> &X.shape[1],
-                &A[0,0], <int*> &A.shape[1],
-                &beta, &temp[0,0], <int*> &temp.shape[1])
+    dgemm('N', 'N', &n, &n, &n, &one, &A[0,0], &n, &sigma[0,0], &n, &zero, &temp_nn[0,0], &n)
+    # dsymm('R','L',&n, &n, &one, &filtered_sigmas[t,0,0], &n, &A[0,0], &n, &zero, &temp_nn[0,0], &n)
+    dcopy(&nn, &sigma_states[0,0], &inc, &sigma_predict[0,0], &inc)
+    dgemm('N', 'T', &n, &n, &n, &one, &temp_nn[0,0], &n, &A[0,0], &n, &one, &sigma_predict[0,0], &n)
 
-        dgemm('N', 'T', <int*> &out.shape[0], <int*> &out.shape[0], <int*> &X.shape[0],
-                &alpha, &A[0,0], <int*> &A.shape[1],
-                &temp[0,0], <int*> &temp.shape[1],
-                &beta, &out[0,0], <int*> &out.shape[0])
+cdef inline void sample_gaussian(
+    # inputs (which get mutated)
+    double[::1] mu, double[::1,:] sigma,
+    # input/output
+    double[::1] randvec,
+    # temps
+    double[::1] temp_n, double[::1,:] temp_nn,
+    ):
+    cdef int n = mu.shape[0]
+    cdef int nn = n*n
+    cdef int inc = 1, info = 0
+    cdef double one = 1.
 
-# NOTE: qform2 is much faster! 1.5 vs 9.75. maybe because it's reading both sides?
-
+    dpotrf('L', &n, &sigma[0,0], &n, &info)
+    dtrmv('L', 'N', 'N', &n, &sigma[0,0], &n, &randvec[0], &inc)
+    daxpy(&n, &one, &mu[0], &inc, &randvec[0], &inc)
 
 def downdate(double[:,::1] R, double[::1] z):
     chol_downdate(R.shape[0],&R[0,0],&z[0])
